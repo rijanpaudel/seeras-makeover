2e726fcb01683f36f95563a2dc40e54c4ad31253
rijanpaudel <paudelrijan369@gmail.com>
2025-02-02 19:28:58 +0545

    Commit

--- backend/router/auth-router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authcontrollers = require("../controllers/auth-controller")
-
-router.route("/").get(authcontrollers.home);
-router.route("/register").post(authcontrollers.register);
-
-module.exports = router;
